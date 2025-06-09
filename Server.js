e741caf0727fa5390431dc9a160c0f1c4b50c2b5
Tushar Sharma <tsharma300601@gmail.com>
2025-06-09 23:10:44 +0530

    Minor fixes

--- Server.js
+++ Server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 const Room = require('./models/Room');
 const axios = require('axios');
 const CustomLRUCache = require('./utils/CustomLRUCache');
+const path = require('path');
+
 
 const compileLimiter = rateLimit({
     windowMs: 10 * 1000, 
@@ -69,6 +71,11 @@ const io = new Server(server, {
         credentials: true,
         allowedHeaders: ['Content-Type']
     }
+});
+
+app.use(express.static('build'));
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname,'build','index.html'));
 });
 
 const userSocketMap = new Map(); 
